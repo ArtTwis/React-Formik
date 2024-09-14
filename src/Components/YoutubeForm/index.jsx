c152727fa72b211f4e1b5e12317bfd42a9b977b1
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-09-14 15:23:07 +0530

    Basic form with validation along with Yup package

--- src/Components/YoutubeForm/index.jsx
+++ src/Components/YoutubeForm/index.jsx
@@ -1,50 +1,106 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+const validate = (values) => {
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = "Field must required";
+  }
+
+  if (!values.email) {
+    errors.email = "Field must required";
+  } else if (
+    !/^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i.test(values.email)
+  ) {
+    errors.email = "Invalid email format";
+  }
+
+  if (!values.channel) {
+    errors.channel = "Field must required";
+  }
+
+  return errors;
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Field must required!"),
+  email: Yup.string()
+    .required("Field must required!")
+    .email("Invalid email format!"),
+  channel: Yup.string().required("Field must required!"),
+});
+
+const onSubmit = (values) => {
+  console.log("values :>> ", values);
+};
 
 const YoutubeForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
+    initialValues,
+    validationSchema,
+    onSubmit,
+    // validate,
   });
-
-  console.log("Form values :>> ", formik.values);
 
   return (
     <div className="m-1 p-1">
       <h1 className="mt-1 mb-1">YoutubeForm</h1>
       <div className="mt-1">
-        <form>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-          />
+        <form onSubmit={formik.handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.name}
+            />
+            {formik.touched?.name && formik.errors?.name ? (
+              <div className="error">{formik.errors.name}</div>
+            ) : null}
+          </div>
 
-          <label htmlFor="name">E-mail</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
+          <div className="form-control">
+            <label htmlFor="name">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched?.email && formik.errors?.email ? (
+              <div className="error">{formik.errors.email}</div>
+            ) : null}
+          </div>
 
-          <label htmlFor="name">Channel</label>
-          <input
-            type="text"
-            name="channel"
-            id="channel"
-            onChange={formik.handleChange}
-            value={formik.values.channel}
-          />
+          <div className="form-control">
+            <label htmlFor="channel">Channel</label>
+            <input
+              type="text"
+              name="channel"
+              id="channel"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.channel}
+            />
+            {formik.touched?.channel && formik.errors?.channel ? (
+              <div className="error">{formik.errors.channel}</div>
+            ) : null}
+          </div>
 
-          <button type="button">Submit</button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
